7a74c23bdf7b38e4fb04d2ddf81d0b900f51d7fa
moono <toilety@gmail.com>
2017-07-31 19:25:03 +0900

    consistent network structure in GAN-MNIST

--- Work-place/GAN-MNIST/GAN-MNIST-tensorflow.py
+++ Work-place/GAN-MNIST/GAN-MNIST-tensorflow.py
@@ -1,7 +1,9 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import pickle as pkl
+# import pickle as pkl
+import time
+
 
 # get data sets
 from tensorflow.examples.tutorials.mnist import input_data
@@ -34,8 +36,11 @@ def discriminator(x, n_units=128, reuse=False, alpha=0.01):
         h1 = tf.layers.dense(x, n_units, activation=None)
         h1 = tf.maximum(alpha * h1, h1) # Leaky ReLU
         
+        h2 = tf.layers.dense(h1, n_units//4, activation=None)
+        h2 = tf.maximum(alpha * h2, h2)
+
         # Logits and sigmoid(0~1) output
-        logits = tf.layers.dense(h1, 1, activation=None)
+        logits = tf.layers.dense(h2, 1, activation=None)
         out = tf.sigmoid(logits)
         
         return out, logits
@@ -48,9 +53,6 @@ hyper parameters
 input_size = 28 * 28 # 28x28 MNIST images flattened
 # Size of latent vector to generator
 z_size = 100
-# Sizes of hidden layers in generator and discriminator
-g_hidden_size = 128
-d_hidden_size = 128
 # learning rate
 learning_rate = 0.002
 # Leak factor for leaky ReLU
@@ -65,11 +67,11 @@ tf.reset_default_graph()
 inputs_real, inputs_z = model_inputs(real_dim=input_size, z_dim=z_size)
 
 # Generator network here(g_model is the generator output)
-g_model = generator(z=inputs_z, out_dim=input_size, n_units=g_hidden_size, reuse=False, alpha=alpha)
+g_model = generator(z=inputs_z, out_dim=input_size, reuse=False, alpha=alpha)
 
 # Disriminator network here
-d_model_real, d_logits_real = discriminator(x=inputs_real, n_units=d_hidden_size, reuse=False, alpha=alpha)
-d_model_fake, d_logits_fake = discriminator(x=g_model, n_units=d_hidden_size, reuse=True, alpha=alpha)
+d_model_real, d_logits_real = discriminator(x=inputs_real, reuse=False, alpha=alpha)
+d_model_fake, d_logits_fake = discriminator(x=g_model, reuse=True, alpha=alpha)
 
 # Calculate losses
 real_labels = tf.ones_like(d_logits_real) * (1 - smooth) # label smoothing
@@ -96,10 +98,12 @@ g_train_opt = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(g_los
 '''
 Training Session
 '''
-batch_size = 100
-epochs = 100
+batch_size = 128
+epochs = 160
 samples = []
 losses = []
+
+start_time = time.time()
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for e in range(epochs):
@@ -130,9 +134,14 @@ with tf.Session() as sess:
         # Sample from generator as we're training for viewing afterwards
         sample_z = np.random.uniform(-1, 1, size=(16, z_size))
         gen_samples = sess.run(
-                       generator(inputs_z, input_size, n_units=g_hidden_size, reuse=True),
+                       generator(inputs_z, input_size, reuse=True),
                        feed_dict={inputs_z: sample_z})
         samples.append(gen_samples)
+
+end_time = time.time()
+total_time = end_time - start_time
+print('Elapsed time: ', total_time)
+# 160 epochs in 313.15 sec
 
 fig1, ax1 = plt.subplots()
 losses = np.array(losses)
@@ -140,6 +149,7 @@ plt.plot(losses.T[0], label='Discriminator')
 plt.plot(losses.T[1], label='Generator')
 plt.title("Training Losses")
 plt.legend()
+plt.savefig('./assets/losses.png')
 
 def view_samples(epoch, samples):
     fig, axes = plt.subplots(figsize=(7,7), nrows=4, ncols=4, sharey=True, sharex=True)
@@ -151,6 +161,7 @@ def view_samples(epoch, samples):
     return fig, axes
 
 fig2, axes2 = view_samples(-1, samples)
+plt.savefig('./assets/last_generated.png')
 
 
 rows, cols = 10, 6
@@ -161,5 +172,6 @@ for sample, ax_row in zip(samples[::int(len(samples)/rows)], axes3):
         ax.imshow(img.reshape((28,28)), cmap='Greys_r')
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
+plt.savefig('./assets/generation_via_epochs.png')
 
-plt.show('hold')
+# plt.show('hold')
